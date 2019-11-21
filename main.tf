bf935f9dbb74e20a8cedbb08283869e382a43044
prdeman <32132835+prdeman@users.noreply.github.com>
2019-11-21 12:26:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
 }
 
 module "web_app_container" {
-   source = "app.terraform.io/deman/web-app-container/azurerm”
+   source = "app.terraform.io/deman/web-app-container/azurerm"
    version = "2.2.1" 
    name = "${var.prefix}"
    port = "80"
