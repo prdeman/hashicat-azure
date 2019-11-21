fad3fc40f017f5d97c3709c6821c1083d258ec47
prdeman <32132835+prdeman@users.noreply.github.com>
2019-11-21 12:23:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = "${var.location}"
 }
+
+module "web_app_container" {
+   source = "app.terraform.io/YOURORGNAME/web-app-container/azurerm”
+   version = "2.2.1" 
+   name = "${var.prefix}"
+   port = "80"
+   https_only = "false"
+   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+   container_type = "docker"
+   container_image = "scarolan/palacearcade"
+}
+output "container_app_url" {
+   value = "http://${module.web_app_container.hostname}"
+}
+
 /*
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
